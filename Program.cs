c293eea8ffb70ae9c5c8e59c2d8151662d285363
Haval-Jalal <Haval.jalal@hotmail.com>
2025-09-06 18:40:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace min_presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
